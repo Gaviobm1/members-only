e594dc9c3ae5b93da4dc52e01e826650595ac2b8
Gavin O Brien <142741214+Gaviobm1@users.noreply.github.com>
2024-07-14 13:26:18 +0200

    Revert "Add routing and controller logic and authentication"
    This reverts commit d2cf630e37b2bd9f46c61013b9e501b5e39ca915.

--- app.js
+++ app.js
@@ -1,29 +1,11 @@
 const express = require("express");
-const session = require("express-session");
 const path = require("path");
 const mongoose = require("mongoose");
 const nconf = require("nconf");
-const sessionStore = require("./utilities/mongoSession");
-const passport = require("passport");
-const localStrategy = require("./utilities/passportStrategy");
-const User = require("./models/userSchema");
 
 nconf.argv().env();
 
 const app = express();
-
-const mongoDb = nconf.get("MONGO_DB_URI");
-
-mongoose
-  .connect(mongoDb)
-  .then((result) =>
-    app.listen(8080, () => {
-      console.log("Listening on 8080");
-    })
-  )
-  .catch((err) => {
-    console.log(err);
-  });
 
 const siteRouter = require("./routes/siteRouter");
 
@@ -32,31 +14,8 @@ app.set("view engine", "ejs");
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
-app.use(
-  session({
-    secret: nconf.get("SECRET"),
-    resave: false,
-    saveUninitialized: false,
-    store: sessionStore,
-    cookie: { maxAge: 1000 * 60 * 60 },
-  })
-);
-app.use(passport.session());
-
-passport.use(localStrategy);
-
-passport.serializeUser((user, done) => {
-  done(null, user.id);
-});
-
-passport.deserializeUser((id, done) => {
-  User.findById(id)
-    .then((user) => {
-      done(null, user);
-    })
-    .catch((err) => {
-      done(err);
-    });
-});
-
 app.use(siteRouter);
+
+app.listen(8080, () => {
+  console.log("Listening on 8080");
+});
--- utilities/customValidation.js
+++
@@ -1,6 +0,0 @@
-const isEqual = (val, { req }) => {
-  const otherVal = req.body.password;
-  return val === otherVal;
-};
-
-module.exports = isEqual;
